4eb98b29ff9e79008a8914e48e3008c86c9a2085
Jacka-1a <27341@stpauls.qld.edu.au>
2025-01-30 15:08:07 +1000

    de

--- Random_number_game.py
+++ Random_number_game.py
@@ -5,13 +5,23 @@
 #tell the user if they are correct
 #keep asking user to guess until they get it right
 import random
+from contextlib import nullcontext
+from mimetypes import guess_extension
+
 number = random.randint(1,100)
 guess = 0
+guesses = 0
 while guess != number:
     guess = int(input("Guess a number between 1 and 100: "))
+    if guess == nullcontext
     if guess > number:
         print("Lower")
+        guesses += 1
     elif guess < number:
         print("Higher")
+        guesses += 1
     else:
         print("Correct")
+        guesses += 1
+# saves the number of guesses and prints it
+print("You took", guesses, "guesses")
